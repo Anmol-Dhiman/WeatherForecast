b09f182bd3e236eab6e230d7478bdad924db0f8d
SherlockVARM <anmoldhiman7111@gmail.com>
2022-04-09 15:34:32 +0530

    the data fetching for the api is done using volley library.

--- app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.Manifest;
+
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
@@ -14,8 +15,10 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+
 import android.view.View;
 import android.widget.Toast;
+
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -31,11 +34,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     private static final int PERMISSION_CODE = 200;
     private ActivityMainBinding binding;
@@ -50,19 +56,82 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
 
-        adapter = new WeatherAdapter(MainActivity.this, weatherModles);
-        binding.recyclerView.setAdapter(adapter);
-        binding.recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
 
         if (checkPermission()) {
             locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
-            String cityName = getCityName(location.getLatitude(), location.getLongitude());
+            getJson(getCityName(location.getLatitude(), location.getLongitude()));
 
         } else {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
         }
+
+        binding.searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getJson(binding.userInput.getText().toString().trim());
+            }
+        });
+
+    }
+
+
+    private void getJson(String cityName) {
+        adapter = new WeatherAdapter(MainActivity.this, weatherModles);
+        binding.recyclerView.setAdapter(adapter);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        String url = "https://api.weatherapi.com/v1/forecast.json?key=cf08f776e9d64f458ba151049222502&q=" + cityName + "&days=1&aqi=no&alerts=no";
+        binding.cityName.setText(cityName);
+        binding.userInput.setText("");
+        Log.d("getJson", "" + url);
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.d("try", "here in the try block");
+                    int temp_cel = response.getJSONObject("current").getInt("temp_c");
+                    binding.temperature.setText(temp_cel + "°c");
+
+                    String conditionText = response.getJSONObject("current").getJSONObject("condition").getString("text");
+                    binding.condition.setText(conditionText);
+
+                    String conditionImage = response.getJSONObject("current").getJSONObject("condition").getString("icon");
+                    Picasso.get().load("https:" + conditionImage).into(binding.conditionIcon);
+
+                    JSONObject forecast = response.getJSONObject("forecast");
+                    JSONObject forecastday = forecast.getJSONArray("forecastday").getJSONObject(0);
+                    JSONArray hour = forecastday.getJSONArray(("hour"));
+                    weatherModles.clear();
+                    for (int i = 0; i < hour.length(); i++) {
+                        Log.d("forLoop", "here we are in the for loop");
+                        JSONObject slot = hour.getJSONObject(i);
+
+                        String time = slot.getString("time");
+                        Double temper = slot.getDouble("temp_c");
+                        String img = slot.getJSONObject("condition").getString("icon");
+                        Double wind = slot.getDouble("wind_kph");
+
+                        weatherModles.add(new WeatherModle(time, "" + temper, img, "" + wind));
+                    }
+
+                    adapter.notifyDataSetChanged();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "There is not any data for the city named " + cityName, Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(jsonObjectRequest);
 
 
     }
@@ -84,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
                     if (city != null && !city.equals("")) {
                         cityName = city;
                     } else {
-                        Toast.makeText(this, "User City Not Found", Toast.LENGTH_SHORT).show();
+
                     }
                 }
             }
--- app/src/main/java/com/example/weatherforecast/WeatherAdapter.java
+++ app/src/main/java/com/example/weatherforecast/WeatherAdapter.java
@@ -13,8 +13,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHolder> {
@@ -42,12 +44,20 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
 
 //        changing the image w.r.t to weather
 
-        Picasso.get().load("http:" + weather.getIcon()).into(holder.conditionImage);
+        Picasso.get().load("https:" + weather.getIcon()).into(holder.conditionImage);
 
 //        changing the date into simple format
 
         SimpleDateFormat input = new SimpleDateFormat("yyyy-mm-dd hh:mm");
         SimpleDateFormat output = new SimpleDateFormat("hh:mm aa");
+        try {
+            Date t = input.parse(weather.getTime());
+
+            holder.time.setText(output.format(t));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
